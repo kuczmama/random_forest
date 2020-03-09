2667d94fabec04ddd17f0e49bec033d2cd2c5ac6
Mark Kuczmarski <mark@mkucz.com>
2020-03-09 18:00:13 +0900

    The beginning of the random_forest gem

---
+++ random_forest/lib/random_forest.rb
@@ -0,0 +1,6 @@
+require "random_forest/version"
+
+module RandomForest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ random_forest/lib/random_forest/version.rb
@@ -0,0 +1,3 @@
+module RandomForest
+  VERSION = "0.1.0"
+end
